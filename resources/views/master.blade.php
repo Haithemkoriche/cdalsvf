245ec36d1d85b7fd5dc7f4ecc2b897d746bb145e
Haithemkoriche <88111725+Haithemkoriche@users.noreply.github.com>
2023-04-12 18:03:05 +0100

    Initial commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <title>@yield('titel')</title>
+</head>
+
+<body>
+    <nav class="navbar sticky-top navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">C D A L S</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa-solid fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ms-auto text-capitalize ">
+                    <a class="nav-link p-lg-3 active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link p-lg-3 " href="#activités">activités</a>
+                    <a class="nav-link p-lg-3 " href="#ateliers">Les ateliers</a>
+                    <a class="nav-link p-lg-3 " href="#evenements">Événements</a>
+                    <a class="nav-link p-lg-3 " href="#about">À propos</a>
+                    <a class="nav-link p-lg-3 " href="#contact">Contact</a>
+                    <a class="nav-link p-lg-3 " href="#">inscrir</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+
+    @yield('content')
+
+
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h3>Nous contacter</h3>
+                    <ul>
+                        <li><i class="fa fa-map-marker"></i> 123 rue des Arts, Alger</li>
+                        <li><i class="fa fa-phone"></i> +213 123 456 789</li>
+                        <li><i class="fa fa-envelope"></i> info@cdls.com</li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h3>Suivez-nous</h3>
+                    <ul class="social-media">
+                        <li><a href="#"><i class="fa-solid fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa-solid fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa-solid fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fa-solid fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h3>Inscription à la newsletter</h3>
+                    <form>
+                        <div class="input-group">
+                            <input type="email" class="form-control" placeholder="Entrez votre email...">
+                            <span class="input-group-btn">
+                                <button class="btn btn-primary" type="button">S'abonner</button>
+                            </span>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="bottom-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p>&copy; 2023 Centre de développement de loisirs et scientifiques. Tous droits réservés.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,231 @@
+@extends('master')
+@section('titel')
+    CDALS
+@endsection
+@section('content')
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active " data-bs-interval="3000">
+                <img src="{{ asset('images/1.jpg') }}" class="d-block w-100 blur carousel-img" alt="...">
+                <div class="carousel-caption">
+                    <h1>Centre de développement d'activités de divertissement scientifique</h1>
+                    <p>C'est une maison de la science et de la connaissance gérée entièrement par l'Association des jeunes
+                        scientifiques amateurs et encadrant un groupe de jeunes ambitieux.</p>
+                    <div class="d-grid row-gap-3"><a href="" class="btn btn-primary p-2">Inscrir</a><a href=""
+                            class="btn btn-info p-2">Contacter</a>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item" data-bs-interval="3000">
+                <img src="{{ asset('images/c-2.jpg') }}" class="d-block w-100 blur carousel-img" alt="...">
+                <div class="carousel-caption">
+                    <h1>Centre de développement d'activités de divertissement scientifique</h1>
+                    <p>C'est une maison de la science et de la connaissance gérée entièrement par l'Association des jeunes
+                        scientifiques amateurs et encadrant un groupe de jeunes ambitieux.</p>
+                    <div class="d-grid row-gap-3"><a href="" class="btn btn-primary p-2">Inscrir</a><a href=""
+                            class="btn btn-info p-2">Contacter</a>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item" data-bs-interval="3000">
+                <img src="{{ asset('images/c-3.jpg') }}" class="d-block w-100 blur carousel-img" alt="...">
+                <div class="carousel-caption">
+                    <h1>Centre de développement d'activités de divertissement scientifique</h1>
+                    <p>C'est une maison de la science et de la connaissance gérée entièrement par l'Association des jeunes
+                        scientifiques amateurs et encadrant un groupe de jeunes ambitieux.</p>
+                    <div class="d-grid row-gap-3"><a href="" class="btn btn-primary p-2">Inscrir</a><a href=""
+                            class="btn btn-info p-2">Contacter</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <!-- start activities -->
+    <section id="activités">
+        <div class="container mb-6 mt-6 pt-5">
+            <h2 style="text-align:center" class="mb-5 mt-5">Nos Activités </h2>
+            <d class="mt-5 row column-gap-auto row-gap-5 ">
+                <div class="col-lg-4 col-md-6">
+                    <div class="card">
+                        <img src="{{ asset('images/c-4.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Activité 1</h5>
+                            <p class="card-text">Some quick example text to build on the Activité 1 and make up the bulk of
+                                the
+                                card's content.</p>
+                            <a href="#" class="btn btn-primary">En savoir plus</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="card">
+                        <img src="{{ asset('images/c-4.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Activité 1</h5>
+                            <p class="card-text">Some quick example text to build on the Activité 1 and make up the bulk of
+                                the
+                                card's content.</p>
+                            <a href="#" class="btn btn-primary">En savoir plus</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="card">
+                        <img src="{{ asset('images/c-4.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Activité 1</h5>
+                            <p class="card-text">Some quick example text to build on the Activité 1 and make up the bulk of
+                                the
+                                card's content.</p>
+                            <a href="#" class="btn btn-primary">En savoir plus</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="card">
+                        <img src="{{ asset('images/c-4.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Activité 1</h5>
+                            <p class="card-text">Some quick example text to build on the Activité 1 and make up the bulk of
+                                the
+                                card's content.</p>
+                            <a href="#" class="btn btn-primary">En savoir plus</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="card">
+                        <img src="{{ asset('images/c-4.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Activité 1</h5>
+                            <p class="card-text">Some quick example text to build on the Activité 1 and make up the bulk of
+                                the
+                                card's content.</p>
+                            <a href="#" class="btn btn-primary">En savoir plus</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="card">
+                        <img src="{{ asset('images/c-4.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Activité 1</h5>
+                            <p class="card-text">Some quick example text to build on the Activité 1 and make up the bulk of
+                                the
+                                card's content.</p>
+                            <a href="#" class="btn btn-primary">En savoir plus</a>
+                        </div>
+                    </div>
+                </div>
+
+        </div>
+    </section>
+    <!-- end activities -->
+    <!-- start ateliers -->
+    <section id="ateliers">
+        <div class="container mb-5 mt-5">
+            <h2 style="text-align:center" class="mb-5 mt-5">Les ateliers proposés</h2>
+            <div class="row column-gap-auto">
+                <div class="col-lg-4 col-md-6">
+                    <div class="atelier">
+                        <img src="{{ asset('images/c-2.jpg') }}" alt="Atelier 1">
+                        <h3>Atelier 1</h3>
+                        <p>Description de l'atelier 1</p>
+                        <a href="#" class="btn btn-primary">S'inscrire</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="atelier">
+                        <img src="{{ asset('images/c-2.jpg') }}" alt="Atelier 2">
+                        <h3>Atelier 2</h3>
+                        <p>Description de l'atelier 2</p>
+                        <a href="#" class="btn btn-primary">S'inscrire</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="atelier">
+                        <img src="{{ asset('images/c-2.jpg') }}" alt="Atelier 3">
+                        <h3>Atelier 3</h3>
+                        <p>Description de l'atelier 3</p>
+                        <a href="#" class="btn btn-primary">S'inscrire</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- end ateliers -->
+    <!-- start evenement -->
+    <section class="evenements" id="evenements">
+        <div class="container">
+            <h2 style="text-align:center" class="pt-5">Événements à venir</h2>
+            <div class="evenements-liste">
+                <div class="evenement">
+                    <img src="{{ asset('images/c-2.jpg') }}" alt="Événement 1">
+                    <h3>Titre de l'événement 1</h3>
+                    <p>Date et heure de l'événement 1</p>
+                </div>
+                <div class="evenement">
+                    <img src="{{ asset('images/c-2.jpg') }}" alt="Événement 2">
+                    <h3>Titre de l'événement 2</h3>
+                    <p>Date et heure de l'événement 2</p>
+                </div>
+                <div class="evenement">
+                    <img src="{{ asset('images/c-2.jpg') }}" alt="Événement 3">
+                    <h3>Titre de l'événement 3</h3>
+                    <p>Date et heure de l'événement 3</p>
+                </div>
+            </div>
+            <a href="evenements.html" class="btn"><u>Voir tous les événements</u></a>
+        </div>
+    </section>
+
+    <!-- end evenement -->
+    <!-- about -->
+    <section id="about">
+        <div class="container">
+            <h2 style="text-align:center">À propos</h2>
+            <img src="{{ asset('images/rouina.jpg') }}" class="rounded mx-auto d-block img-fluid" alt=""
+                srcset="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, libero at auctor congue, enim tellus
+                venenatis magna, in venenatis lectus tellus eu sapien. Proin quis neque rutrum, lobortis felis eu,
+                vestibulum nibh. Nullam interdum sem ac nunc sodales, nec finibus justo sagittis.</p>
+            <a href="about.html" class="btn btn-primary">En savoir plus</a>
+        </div>
+    </section>
+
+    <!-- end about -->
+    <!-- contact -->
+    <section id="contact">
+        <div class="container">
+            <h2 style="text-align:center">Contact</h2>
+            <form action="contact.php" method="post">
+                <div class="form-group">
+                    <label for="name">Nom</label>
+                    <input type="text" class="form-control" id="name" name="name" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Adresse email</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+                <div class="form-group">
+                    <label for="message">Message</label>
+                    <textarea class="form-control" id="message" name="message" rows="5" required></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Envoyer</button>
+            </form>
+        </div>
+    </section>
+
+    <!-- end contact -->
+@endsection
